4c85437ef2c20c95d45ff5a0072fdc00bd17a979
zf <574676312@qq.com>
2026-01-27 17:40:04 +0800

    代理账号提现

--- pakgopay/src/views/pages/WithdrawlHistory.vue
+++ pakgopay/src/views/pages/WithdrawlHistory.vue
@@ -1,29 +1,10 @@
 <script setup>
 
 import SvgIcon from "@/components/SvgIcon/index.vue";
-import {getFormateDate} from "@/api/common.js";
 </script>
 
 <template>
   <div class="main-title">代理账号</div>
-  <div style="display: flex;align-items: inherit;margin-top: 1%;margin-bottom:0">
-    <el-form-item style="margin-left: 2%;">
-      <template #label>
-          <span style="color: black;font-size: small;align-items: center;">
-            统计币种:
-          </span>
-      </template>
-      <el-select
-          style="width: 100px;align-items: center;text-align: center;"
-          :options="currencyOptions"
-          :props="currencyProps"
-          default-first-option
-          v-model="filterbox.currency"
-          @change="handleCurrencyChange"
-          filterable
-      />
-    </el-form-item>
-  </div>
   <!--  <div style="display: flex;height: 12vh;justify-content: space-between;margin-right: 10%;margin-left: 10%;">
       <el-card style="width: 30%;height: 100%;margin-top: 2%;">
         <div style="display: flex;">
@@ -182,7 +163,14 @@ import {getFormateDate} from "@/api/common.js";
         </el-table-column>
         <el-table-column
             v-slot="{row}"
-            label="代理提现账户"
+            label="代理钱包"
+            align="center"
+        >
+          <div>{{ row.walletName }}</div>
+        </el-table-column>
+        <el-table-column
+            v-slot="{row}"
+            label="代理钱包地址"
             align="center"
         >
           <div>{{ row.walletAddr }}</div>
@@ -339,13 +327,103 @@ import {getFormateDate} from "@/api/common.js";
       <el-button type="primary" @click="submit('createAccountForm')">确 定</el-button>
     </div>
   </el-dialog>
+  <!-- withdraw dialog -->
+  <el-dialog
+      :title="dialogWithdrawTitle"
+      v-model="dialogWithdrawVisible"
+      class="dialog"
+      center
+      width="50%"
+      height="50%"
+  >
+    <el-form :model="withdrawOrderInfo" label-width="100%" class="form" ref="withdrawOrderInfoForm"
+             :rules="withdrawOrderRule">
+      <div class="el-form-line">
+        <el-form-item label="代理名称:" label-width="150px" prop="merchantAgentId">
+          <el-select
+              :options="agentAccountOptions"
+              :props="agentAccountProps"
+              style="width: 200px"
+              v-model="withdrawOrderInfo.merchantAgentId"
+              @change="handleAgentChange"
+              :disabled="filterAvaiable"
+          >
+          </el-select>
+        </el-form-item>
+      </div>
+      <div class="el-form-line">
+        <el-form-item label="币种:" label-width="150px" prop="availableAmount">
+          <el-select v-model="withdrawOrderInfo.currency" style="width: 200px"
+                     :options="currencyOptions"
+                     :props="currencyProps"
+                     @change="handleWithdrawCurrencyChange"
+          />
+        </el-form-item>
+      </div>
+      <div class="el-form-line">
+        <el-form-item label="可用余额:" label-width="150px" prop="availableAmount">
+          <el-input disabled v-model="withdrawOrderInfo.availableAmount" style="width: 200px"></el-input>
+        </el-form-item>
+      </div>
+      <div class="el-form-line">
+        <el-form-item label="钱包地址:" label-width="150px" prop="walletAddr">
+          <el-select
+              v-model="withdrawOrderInfo.walletAddr"
+              style="width: 200px"
+              :options="agentAccountOptions"
+              :props="agentWalletProps"
+              filterable
+          />
+        </el-form-item>
+      </div>
+      <div class="el-form-line">
+        <el-form-item label="提现金额:" label-width="150px" prop="amount">
+          <el-input type="number" v-model="withdrawOrderInfo.amount" style="width: 200px"/>
+        </el-form-item>
+      </div>
+      <!--      <div class="el-form-line">
+              <el-form-item label="谷歌验证码:" label-width="150px" prop="googleCode">
+                <el-input v-model="withdrawOrderInfo.googleCode" style="width: 200px"/>
+              </el-form-item>
+            </div>-->
+    </el-form>
+    <div slot="footer" class="dialog-footer">
+      <el-button @click="cancelWithdraw('withdrawOrderInfoForm')">取 消</el-button>
+      <el-button type="primary" @click="submitWithdraw('withdrawOrderInfoForm')">确 定</el-button>
+    </div>
+  </el-dialog>
+  <el-dialog
+      :title="confirmDialogTitle"
+      v-model="confirmDialogVisible"
+      class="dialog"
+      center
+      width="30%"
+      height="200px"
+  >
+    <el-form ref="confirmDataForm" :rules="confirmRule" :model="confirmData" style="height:100px;margin-top: 20px">
+      <el-row>
+        <el-col :span="24" style="display: flex;justify-content: center;justify-items: center;align-items: center;">
+          <div>
+            <el-form-item label="谷歌验证码:" label-width="150px" prop="googleCode">
+              <el-input v-model="confirmData.googleCode" style="width: 200px"/>
+            </el-form-item>
+          </div>
+        </el-col>
+      </el-row>
+    </el-form>
+    <div slot="footer" class="dialog-footer" style="margin-right: 3%;height: 30px;">
+      <el-button @click="cancelConfirmDialog('confirmDataForm')">取 消</el-button>
+      <el-button type="primary" @click="submitConfirm('confirmDataForm')">确 定
+      </el-button>
+    </div>
+  </el-dialog>
 </template>
 
 <script>
 import {
-  createAgentAccountInfo, exportAgentAccount, exportMerchantAccount,
+  createAgentAccountInfo, createStatementeOrderApply, exportAgentAccount, exportMerchantAccount,
   getAgentAccountInfo, getAgentInfo,
-  getAllCurrencyType, modifyAgentAccountInfo,
+  getAllCurrencyType, getMerchantAccount, getMerchantInfo, modifyAgentAccountInfo,
   modifyAgentInfo
 } from "@/api/interface/backendInterface.js";
 import {exportExcel, getAgentAccountTitle, getFormateTime, getMerchantAccountTitle, loadingBody} from "@/api/common.js";
@@ -353,8 +431,32 @@ import {exportExcel, getAgentAccountTitle, getFormateTime, getMerchantAccountTit
 export default {
   name: 'WithdrawlHistory',
   data() {
+
+    const amountValidate = (rule, value, callback) => {
+      if (!value || value === 0) {
+        callback(new Error("amount is required"));
+      } else if (value > this.withdrawOrderInfo.availableAmount) {
+        callback(new Error("amount is more than your available amount"));
+      } else {
+        callback();
+      }
+    }
     return {
+      amountInfo: {},
+      filterAvaiable: false,
       timeZoneKey: localStorage.getItem("timeZone") || "UTC+8",
+      withdrawOrderInfo: {},
+      agentAccountOptions: [],
+      agentAccountProps: {
+        value: 'merchantAgentId',
+        label: 'name',
+      },
+      agentWalletProps: {
+        value: 'walletAddr',
+        label: 'walletAddr',
+      },
+      dialogWithdrawVisible: false,
+      dialogWithdrawTitle: '',
       tableKey: 0,
       activeTool: '1',
       agentOptions: [],
@@ -400,7 +502,24 @@ export default {
         googleCode: {
           required: true, messages: 'you need to type walletAddr', trigger: 'blur'
         }
-      }
+      },
+      withdrawOrderRule: {
+        merchantAgentId: {
+          required: true, trigger: 'blur',
+        },
+        walletAddr: {
+          required: true, trigger: 'blur'
+        },
+        amount: {
+          required: true, trigger: 'blur', validator: amountValidate
+        },
+        availableAmount: {
+          required: true, trigger: 'blur'
+        }
+        /* googleCode: {
+           required: true, trigger: 'blur'
+         }*/
+      },
     }
   },
   methods: {
@@ -437,8 +556,17 @@ export default {
         await exportExcel(res, fileName, this)
       })
     },
-    handleAgentChange(val) {
-      this.createAgentAccountModel.name = val;
+    handleAgentChange(value) {
+      let opt = [];
+      this.agentAccountOptions.find((item) => {
+        //return item.merchantAgentId === value;
+        if (item.merchantAgentId === value) {
+          opt.push(item);
+        }
+      });
+      this.withdrawOrderInfo.merchantAgentName = opt[0].name;
+      //this.withdrawOrderInfo.walletAddr = opt.walletAddr;
+      this.agentAccountOptions = Object.assign([], opt)
     },
     search() {
       this.filterbox.isNeedCardData = true
@@ -447,16 +575,18 @@ export default {
         this.filterbox.endTime = this.filterbox.filterDateRange[1]/1000
       }
       const loadingInstance = loadingBody(this, 'agentAccountTable')
-      getAgentAccountInfo(this.filterbox).then(res => {
+      this.updateAgentAccount(loadingInstance)
+      /*getAgentAccountInfo(this.filterbox).then(res => {
         if (res.status === 200 && res.data.code === 0) {
           let allData = JSON.parse(res.data.data)
           this.withdrawAccountData = allData.withdrawalAccountsDtoList;
+          this.agentAccountOptions = allData.withdrawalAccountsDtoList
           this.totalCount = allData.totalNumber
           this.pageNo = allData.pageNo
           this.pageSize = allData.pageSize
           this.tableKey++
 
-          /*if(allData.cardInfo) {
+          /!*if(allData.cardInfo) {
             const cardInfo = allData.cardInfo[this.filterbox.currency]
             this.agentTotalAmount = cardInfo.total
             this.agentFreezeAmount = cardInfo.frozen
@@ -465,7 +595,7 @@ export default {
             this.agentTotalAmount =  this.currencyIcons[this.currency] + 0
             this.agentFreezeAmount = this.currencyIcons[this.currency] + 0
             this.agentAvailableBalance = this.currencyIcons[this.currency] + 0
-          }*/
+          }*!/
 
         } else if (res.status === 200 && res.data.code !== 0) {
           this.$notify({
@@ -494,13 +624,24 @@ export default {
           position: 'bottom-right',
           type: 'error'
         })
-      })
+      })*/
     },
     addWithdrawlAccount() {
       this.createAccountVisible = true
       this.createAccountTitle = '新增代理账号'
       this.dialogType = 'create'
       this.submitType = 'create'
+    },
+    createWithdrawlApply() {
+// set withdraw merchant
+      if(this.roleName === 'merchant') {
+        const merchantInfo = this.merchantAccountOptions[0]
+        this.withdrawOrderInfo.merchantAgentId = merchantInfo.merchantAgentId
+        this.handleMerchantChange(this.withdrawOrderInfo.merchantAgentId)
+      }
+      this.dialogWithdrawVisible = true
+      this.dialogWithdrawTitle = '提现'
+      this.withdrawOrderInfo.orderType = 2
     },
     editAgentAccount(row) {
       this.createAgentAccountModel = {}
@@ -617,13 +758,122 @@ export default {
       this.createAccountTitle = ''
       this.dialogType = ''
     },
-    handleCurrencyChange() {
-      this.currency = this.filterbox.currency;
-      this.currencyIcon = this.currencyIcons[this.currency]
-      this.search()
+    handleWithdrawCurrencyChange(val) {
+      let opt = {}
+      this.amountInfo ? opt = this.amountInfo[val] : opt = null
+
+      opt ? this.withdrawOrderInfo.availableAmount = opt.available : this.withdrawOrderInfo.availableAmount = 0
     },
+    cancelWithdraw(form) {
+      this.$refs[form].resetFields()
+      this.dialogWithdrawTitle = ''
+      this.dialogWithdrawVisible = false
+      this.updateAgentAccount(null)
+    },
+    submitWithdraw(form) {
+      this.$refs[form].validate(validate => {
+        if (validate) {
+          this.dialogWithdrawVisible = false
+          this.dialogWithdrawTitle = ''
+          this.confirmData =  Object.assign({}, this.withdrawOrderInfo)
+          this.confirmDialogTitle = '谷歌验证'
+          this.confirmDialogVisible = true
+          this.$refs[form].resetFields()
+        }
+      })
+    },
+    submitConfirm(form) {
+      let orderMessage = form.orderType === 1 ? 'Recharge' : form.orderType === 2 ? 'Withdraw' : 'Manual Account Adjustment'
+      this.$refs[form].validate(validate => {
+        if (validate) {
+          createStatementeOrderApply(this.confirmData).then(res => {
+            this.confirmDialogTitle=''
+            this.confirmDialogVisible = false
+            if (res.status === 200 && res.data.code === 0) {
+              this.$refs[form].resetFields()
+              this.$notify({
+                title: 'Success',
+                type: 'success',
+                message: 'Create '+orderMessage+' Order Successfully.',
+                duration: 3000,
+                position: 'bottom-right'
+              })
+              this.search()
+              this.getNewstMerchantInfo()
+            } else if (res.status === 200 && res.data.code !== 0) {
+              this.$notify({
+                title: 'Failed',
+                type: 'error',
+                message: res.data.message,
+                duration: 3000,
+                position: 'bottom-right'
+              })
+            } else {
+              this.$notify({
+                title: 'Error',
+                type: 'error',
+                message: 'somethind went wrong, try it again',
+                duration: 3000,
+                position: 'bottom-right'
+              })
+            }
+          })
+        }
+      })
+    },
+    updateAgentAccount(loadingInstance) {
+      getAgentAccountInfo(this.filterbox).then(res => {
+        if (res.status === 200 && res.data.code === 0) {
+          let allData = JSON.parse(res.data.data)
+          this.withdrawAccountData = allData.withdrawalAccountsDtoList;
+          this.agentAccountOptions = allData.withdrawalAccountsDtoList
+          this.totalCount = allData.totalNumber
+          this.pageNo = allData.pageNo
+          this.pageSize = allData.pageSize
+          this.tableKey++
+        } else if (res.status === 200 && res.data.code !== 0) {
+          this.$notify({
+            title: 'Error',
+            type: 'error',
+            message: res.data.message,
+            duration: 3000,
+            position: "bottom-right"
+          })
+        } else {
+          this.$notify({
+            title: 'Error',
+            type: 'error',
+            message: 'something wrong happened, try it again',
+            duration: 3000,
+            position: "bottom-right"
+          })
+        }
+        loadingInstance !== null ? loadingInstance.close() : ''
+      }).catch(err => {
+        loadingInstance !== null ? loadingInstance.close() : ''
+        this.$notify({
+          title: 'Error',
+          type: 'error',
+          message: err.message,
+          duration: 3000,
+          position: "bottom-right"
+        })
+      })
+    },
+    getNewstAgentInfo() {
+      getAgentInfo({merchantUserName: this.filterbox.name, isNeedCardData: true}).then(res => {
+        if (res.status === 200 && res.data.code === 0) {
+          let allData = JSON.parse(res.data.data)
+          this.agentOptions = allData.agentInfoDtoList
+          this.amountInfo = allData.cardInfo
+        }
+      })
+    }
   },
   async mounted() {
+    this.roleName = localStorage.getItem("roleName")
+    this.filterbox.name = this.roleName=== 'agent'? localStorage.getItem('userName') : null
+    this.filterAvaiable = this.roleName === 'agent'? true : false
     this._timeZoneListener = (event) => {
       this.timeZoneKey = event.detail || localStorage.getItem("timeZone") || "UTC+8";
     };
@@ -645,11 +895,7 @@ export default {
       }
     })
 
-    getAgentInfo({pageSize: 1000}).then(res => {
-      if (res.status === 200 && res.data.code === 0) {
-        this.agentOptions = JSON.parse(res.data.data).agentInfoDtoList
-      }
-    })
+    this.getNewstAgentInfo()
     this.search()
   },
   beforeUnmount() {
@@ -687,6 +933,11 @@ export default {
   text-align: center;
 }
 
+:deep(.el-select .el-input__inner),
+:deep(.el-select .el-select__selected-item) {
+  text-align: center;
+}
+
 .addDialog {
   display: flex;
   justify-content: center;
@@ -696,6 +947,19 @@ export default {
 .dialog-footer {
   display: flex;
   float: right;
-  margin-right: 5%;
+}
+
+.form {
+  margin-top: 3%;
+  height: 300px;
+}
+
+.el-form-line {
+  display: flex;
+  justify-content: center;
+}
+
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none !important;
 }
 </style>
